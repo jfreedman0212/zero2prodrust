6d3f5079a54250257e2576c425a7566ce0007d66
Josh Freedman <joshfreedman@pm.me>
2021-08-01 13:09:42 -0400

    Refactor app to use TCP Listener (so any port can be used) Refactor tests to spin app up on random port (from Port 0)

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,14 +1,16 @@
+use std::net::TcpListener;
+
 /// Tests that we can hit the /health_check endpoint successfully
 /// and that the response is correct
 #[actix_rt::test]
 async fn health_check_works() {
     // Arrange (create the app and the request client)
-    spawn_app();
+    let base_url = spawn_app();
     let client = reqwest::Client::new();
 
     // Act (send the request and track the response)
     let response = client
-        .get("http://localhost:8080/health_check")
+        .get(format!("{}/health_check", &base_url))
         .send()
         .await
         .expect("Failed to execute Health Check Request");
@@ -19,7 +21,10 @@ async fn health_check_works() {
 }
 
 /// Helper function that spins up our application in a background task (using Tokio)
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to start server");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Cannot bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to start server");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
