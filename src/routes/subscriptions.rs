f84ff49921439b79a72a09155c33306f7debc8de
Josh Freedman <joshfreedman@pm.me>
2021-08-01 15:36:29 -0400

    Attempt to fix CI Add --locked to SQLX install Allow dead code on FormData struct (for now)

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,6 +3,7 @@ use actix_web::{web, HttpResponse};
 /// Data model for what we need to subscribe a user to our mailing list.
 /// We need their email (of course) and their name. Both fields are required.
 #[derive(serde::Deserialize)]
+#[allow(dead_code)] // FIXME: remove this once we actually implement subscribe call!
 pub struct FormData {
     email: String,
     name: String,
