bd8cd4638349fd7dd24e5dafb281b7131540ced4
Josh Freedman <joshfreedman@pm.me>
2021-08-02 20:33:34 -0400

    Fix tests by spinning up new logical database in each test

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,29 +1,50 @@
 use config::ConfigError;
 
+/// Represents the total configuration for the application
 #[derive(serde::Deserialize)]
 pub struct Settings {
+    /// Contains database settings (see type for more details)
     pub database: DatabaseSettings,
+    /// Port that the application will listen on
     pub application_port: u16,
 }
 
+/// Contains all settings necessary to communicate with a Database
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
+    /// Database Username
     pub username: String,
+    /// Database Password
     pub password: String,
+    /// Port the database is listening on
     pub port: u16,
+    /// Hostname that the database is hosted on
     pub host: String,
+    /// Name of the database to connect to
     pub database_name: String,
 }
 
 impl DatabaseSettings {
+    /// Constructs a full Postgres connection string
     pub fn connection_string(&self) -> String {
         format!(
             "postgres://{}:{}@{}:{}/{}",
             self.username, self.password, self.host, self.port, self.database_name
         )
     }
+
+    /// Constructs a Postgres connection string without the DB name
+    /// To use this, you'll need to provide a name to use
+    pub fn connection_string_without_db_name(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
+    }
 }
 
+/// Reads from a "configuration.yml" file and returns a `Result` containing either
+/// the populated `Settings` or an error reading from the config file.
 pub fn get_configuration() -> Result<Settings, ConfigError> {
     let mut settings = config::Config::default();
     settings.merge(config::File::with_name("configuration"))?;
--- tests/utils.rs
+++ tests/utils.rs
@@ -1,6 +1,7 @@
-use sqlx::PgPool;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
-use zero2prod::get_configuration;
+use uuid::Uuid;
+use zero2prod::{get_configuration, DatabaseSettings};
 
 pub struct TestApp {
     pub address: String,
@@ -13,11 +14,10 @@ pub async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Cannot bind to random port");
     let port = listener.local_addr().unwrap().port();
     // load application configuration for the DB connection string
-    let config = get_configuration().expect("Failed to read configuration file");
+    let mut config = get_configuration().expect("Failed to read configuration file");
+    config.database.database_name = Uuid::new_v4().to_string();
     // build the connection pool
-    let connection_pool = PgPool::connect(&config.database.connection_string())
-        .await
-        .expect("Failed to establish connection to Postgres");
+    let connection_pool = configure_database(&config.database).await;
     // spin up the server and run it as a background task
     let server = zero2prod::run(listener, connection_pool.clone()).expect("Failed to start server");
     let _ = tokio::spawn(server);
@@ -26,4 +26,27 @@ pub async fn spawn_app() -> TestApp {
         address: format!("http://127.0.0.1:{}", port),
         connection_pool,
     }
+}
+
+/// Helper function that spins up a new logical database
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    // create a new logical database with a random name for testing purposes
+    let mut connection = PgConnection::connect(&config.connection_string_without_db_name())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.database_name))
+        .await
+        .expect("Failed to create database");
+    // set up the connection pool
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to establish connection to Postgres");
+    // run all migrations on this database
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate");
+    // return the connection pool
+    connection_pool
 }
